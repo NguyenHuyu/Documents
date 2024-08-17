3188b53a88dd9bfc5a2f7d4f89c2d87fd925e2bb
Nguyen Minh Huy <nguyenminhhuyk13@siu.edu.vn>
2024-08-17 10:55:51 +0700

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div className="flex items-center justify-center h-full">
+      <h1>Welcome to Shadcn Components</h1>
+    </div>
+  );
+};
+
+export default HomePage;
